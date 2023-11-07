5f1277a5aa26591d3289f8566f5420f6a92cd630
atifrit <adamtifrit@gmail.com>
2023-11-07 13:00:05 -0800

    added landing page and updated navbar

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserBookings } from "../../store/bookings";
 import { Link, Redirect, useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-
+import './LandingPage.css';
 
 
 const LandingPage = () => {
@@ -14,17 +14,18 @@ const LandingPage = () => {
 
     return (
         <div className='landing-page'>
-      <div className='nav'>
-        {/* <Navigation /> */}
-      </div>
-      <div className='landing-container'>
-        <img className='landing-image' src={"https://cdn.robinhood.com/assets/generated_assets/brand/_next/static/images/product_hero_invest__91b9077cf4788b508a013b9dda8c3ffe4d4fff969655c212a0201c9533237d46.png"} />
-        <div className='title-container'>
-          <div className='title-text'>Welcome to Canaryhood</div>
-          <div className='sub-title-text'>Join a new generation of investors</div>
+            <div className='nav'>
+                {/* <Navigation /> */}
+            </div>
+            <div className='landing-container'>
+                <img className='landing-image' src={"https://www.spotfreecar.com/wp-content/uploads/2022/05/los-angeles-car-wash-package-memberships-570x420.jpg"} />
+                <div className='title-container'>
+                    <div className='title-text'>Gorilla Buff Auto Detailing</div>
+                    <div className='sub-title-text'>Showroom quality service for your valued ride</div>
+                </div>
+                <img className='landing-image' src={"https://www.godetail.com/wp-content/uploads/2023/01/can-car-wash-damage-brakes.jpg"} />
+            </div>
         </div>
-      </div>
-    </div>
     )
 }
 
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,23 +1,69 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../store/session';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+
+
+function Navigation(){
+	const dispatch = useDispatch();
 	const sessionUser = useSelector(state => state.session.user);
 
+
+	const handleLogout = () => {
+		dispatch(logout());
+	  };
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div className='nav-container'>
+		<div className='left-container'>
+		  <div className='logo-container'>
+			<NavLink to="/" id='landing-logo'>
+			  <img src={'https://static.vecteezy.com/system/resources/thumbnails/010/486/942/small/blue-car-wash-auto-detailing-logo-vector.jpg'} alt="logo" />
+			</NavLink>
+		  </div>
+		  <div className='github-links'>
+			<div className='github-developer'>
+			  <a href="https://github.com/atifrit">
+			  <div className='github'>
+				<img
+				  id="github-logo"
+				  src={'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png'}
+				  alt="github logo"
+				></img>
+			  <div className='github-text'>Adam Tifrit</div>
+			  </div>
+			  </a>
+			</div>
+		  </div>
+		</div>
+		<div className="right-nav-container">
+		  {sessionUser ? (
+			<><button onClick={handleLogout} className="login">
+			  Logout
+			</button>
+			  <NavLink className="signup" to="/bookings">
+				Bookings
+			  </NavLink>
+			  <NavLink className="signup" to="/profilepage">
+				Profile Page
+			  </NavLink>
+			  </>
+
+		  ) : (
+			<>
+			  <NavLink className="login" to="/login">
+				Log in
+			  </NavLink>
+			  <NavLink className="signup" to="/signup">
+				Sign up
+			  </NavLink>
+			</>
+		  )}
+		</div>
+	  </div>
+
+
 	);
 }
 
