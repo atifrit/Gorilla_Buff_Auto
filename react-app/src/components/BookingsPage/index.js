c1dd579a40ad732950d39dec2c679a26afd9230e
atifrit <adamtifrit@gmail.com>
2023-11-06 20:24:29 -0800

    added bookings information page

--- react-app/src/components/BookingsPage/index.js
+++ react-app/src/components/BookingsPage/index.js
@@ -4,6 +4,7 @@ import { getUserBookings } from "../../store/bookings";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import './user_booking_page.css';
 
+
 const BookingsPage = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.session.user)
@@ -22,14 +23,32 @@ const BookingsPage = () => {
     return (
         <>
             <div className='user_bookings_page_container'>
+            <h2>Upcoming Appointments</h2>
+                <div className='upcoming_appointments'>
                 {bookings.user_bookings.map((booking) => {
-                    return (
-                        <div className='bookingDetail'>
-                            <Link
-                                to={`/bookings/${booking.id}`}>{booking.id}</Link>
-                        </div>
-                    )
+                    if (new Date().getTime() < new Date(booking.appointment_date).getTime()) {
+                        return (
+                            <div className='bookingDetail'>
+                                <Link
+                                    to={`/bookings/${booking.id}`}><div>{booking.appointment_date} {booking.car_type} {booking.servive_type}</div></Link>
+                            </div>
+                        )
+                    } else return null
                 })}
+                </div>
+                <h2>Past Appointments</h2>
+                <div className="past_bookings">
+                {bookings.user_bookings.map((booking) => {
+                    if (new Date().getTime() >= new Date(booking.appointment_date).getTime()) {
+                        return (
+                            <div className='bookingDetail'>
+                                <Link
+                                    to={`/bookings/${booking.id}`}><div>{booking.appointment_date} {booking.car_type} {booking.servive_type}</div></Link>
+                            </div>
+                        )
+                    } else return null
+                })}
+                </div>
             </div>
         </>
     )
